4f8b59412eea53471d9756f2ff4db128e0aadfd7
Grisyt <hxl164@outlook.com>
2022-05-20 14:47:53 -0600

    Project Object

---
+++ PPMTool/src/main/java/com/example/ppmtool/domain/Project.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2022 tu.cn All right reserved. This software is the
+ * confidential and proprietary information of tu.cn ("Confidential
+ * Information"). You shall not disclose such Confidential Information and shall
+ * use it only in accordance with the terms of the license agreement you entered
+ * into with Tu.cn
+ */
+package com.example.ppmtool.domain;
+
+/**
+ * @author 97679
+ * @date 2022/5/20 14:46
+ */
+public class Project {
+}
