d221d3db9bd2702794bac056089edb067b7f4831
Grisyt <hxl164@outlook.com>
2022-05-20 14:38:54 -0600

    first commit

---
+++ PPMTool/src/test/java/com/example/ppmtool/PpmToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ppmtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
