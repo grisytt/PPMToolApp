726498aa6022633e1246eda21aeff55e7b65eb1d
Grisyt <hxl164@outlook.com>
2022-06-27 17:00:16 -0600

    Project Object

--- PPMTool/src/main/java/com/example/ppmtool/security/JwtAuthenticationFilter.java
+++ PPMTool/src/main/java/com/example/ppmtool/security/JwtAuthenticationFilter.java
@@ -1,10 +1,3 @@
-/*
- * Copyright 2022 tu.cn All right reserved. This software is the
- * confidential and proprietary information of tu.cn ("Confidential
- * Information"). You shall not disclose such Confidential Information and shall
- * use it only in accordance with the terms of the license agreement you entered
- * into with Tu.cn
- */
 package com.example.ppmtool.security;
 
 import com.example.ppmtool.domain.User;
